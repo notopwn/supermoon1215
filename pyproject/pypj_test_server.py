c4f3d86b83991cf83600cb242982835f6bb25d08
notopwn <notopwn@icloud.com>
2020-10-22 18:42:05 +0900

    testing saving
    testing new server script saving

--- pyproject/pypj_test_server.py
+++ pyproject/pypj_test_server.py
@@ -10,4 +10,12 @@ def response(key) :
 
 def handler(clientsock, addr):
     while True:
+        data = clientsock.recv(r_buff)
+        print('data : ' + repr(data))
+        if not data : break
+        clientsock.send(response('').encode())
+        print('sent : ' + repr(response('')))
 
+if __name__ == '__main__':
+    end_point = (host_addr, port)
+    serversock = socket(AF_INET, SOCK_STREAM)
