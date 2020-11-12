4f0a32414babe24bd00142b00806779e6a2d7925
notopwn <notopwn@icloud.com>
2020-11-12 17:46:37 +0900

    multichatserver updates
    but it doesnt working at client, when client send something, it goes error

--- pyproject/chatclient.py
+++ pyproject/chatclient.py
@@ -1,5 +1,21 @@
 import socket
 
-host = 'localhost'
-port = 5000
+HOST = 'localhost'
+PORT = 5000
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM)as sock:
+    sock.connect((HOST,PORT))
+
+    while True:
+        msg = input('enter message : ')
+        sock.sendall(msg.encode())
+        data = sock.recv(1024)
+        if msg!=data.decode():
+            print('data from server : [%s]' % data.decode())
+
+        if msg == '/quit':
+            sock.sendall(msg.encode())
+            break
+
+
+print('quitting client...')
--- pyproject/multichatserver.py
+++ pyproject/multichatserver.py
@@ -1,18 +1,21 @@
+PORT = 5000
+HOST = 'localhost'
+
 from socket import *
 from threading import *
 
-
 class MultiChatServer:
     clients = []
-    finalrecievedmessage=""
+    final_received_message = ""
 
     def __init__(self):
+        global HOST,PORT
         self.s_sock = socket(AF_INET, SOCK_STREAM)
-        self.ip=''
-        self.port = 5000
-        self.s_sock.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-        self.s_sock.bind((self.ip,self.port))
-        print("temporally waiting for connection")
+        self.ip = ''
+        self.port = PORT
+        self.s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.s_sock.bind((self.ip, self.port))
+        print("temporally wating for connection from clients")
         self.s_sock.listen(100)
         self.accept_client()
 
@@ -21,34 +24,33 @@ class MultiChatServer:
             client = c_socket,(ip,port) = self.s_sock.accept()
             if client not in self.clients:
                 self.clients.append(client)
-            print(ip,':',str[port],'connected')
-            t = Thread(target = self.recieve_messages, args = (c_socket,))
-            t.start()
+            print(ip,':',str(port),'is connected')
+        t = Thread(target=self.receive_messages, args=(c_socket,))
+        t.start()
 
-    def recieve_messages(self, c_socket):
+    def receive_messages(self, c_socket):
         while True:
             try:
-                incoming_message = c_socket.recv(1024)
+                incoming_message = c_socket.recv(2048)
                 if not incoming_message:
                     break
             except:
-                print("connection lost")
-
+                print("connection quit")
             else:
-                self.finalrecievedmessage=incoming_message.decode('utf-8')
-                print(self.finalrecievedmessage)
+                self.final_received_message = incoming_message.decode('utf-8')
+                print(self.final_received_message)
                 self.send_all_clients(c_socket)
-            c_socket.close()
+            c_socket.close
 
-    def send_all_clients(self, senders_socket):
-        for client in self.clients:
-            socket,(ip,port) = client
-            if socket is not senders_socket:
-                try:
-                    socket.sendall(self.finalrecievedmessage.encode('utf-8'))
-                except:
-                    self.clients.remove(client)
-                    print("{},{} connection quit".format(ip.port))
+        def send_all_clients(self, senders_socket):
+            for client in self.clients:
+                socket,(ip,port) = client
+                if socket is not senders_socket:
+                    try:
+                        socket.sendall(self.final_received_message.encode('utf-8'))
+                    except:
+                        self.clients.remove(client)
+                        print("{},{} disconnected".format(ip,port))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     MultiChatServer()
